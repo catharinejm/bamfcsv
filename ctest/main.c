260b9570e128f099dcf1aaa3d67366e11a76e129
Jon Distad <jon.distad@gmail.com>
2011-03-29 10:25:55 -0400

    Add pipe-delimited output of parsed CSV to ctest

--- ctest/main.c
+++ ctest/main.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "../bamfcsv.h"
 
 int main(int argc, char *argv[]) {
-  parse(argv[1]);
+  struct s_Parsed *results = parse(argv[1]);
+
+  int i, j;
+  for (i = 0; i < results->count; ++i) {
+    printf("Line %d: ", i+1);
+    for (j = 0; j < results->rows[i].count; ++j) {
+      printf("%s", results->rows[i].cells[j]);
+      if (j + 1 < results->rows[i].count) printf(" | ");
+    }
+    printf("\n");
+  }
   return 0;
 }
