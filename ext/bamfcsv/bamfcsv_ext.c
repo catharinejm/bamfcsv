31073a70e2f3ca9f1ee8b20ca3cc757544aeddd5
Jon Distad <jon.distad@gmail.com>
2011-04-16 01:17:44 -0400

    First attempt at not generating the parse tree

--- ext/bamfcsv/bamfcsv_ext.c
+++ ext/bamfcsv/bamfcsv_ext.c
@@ -1,124 +1,46 @@
+#include <ruby/ruby.h>
 #include <stdlib.h>
-#include "bamfcsv_ext.h"
 
-struct bamfcsv_Cell *bamfcsv_alloc_cell(struct bamfcsv_Row *row, struct bamfcsv_Cell *prev_cell) {
+VALUE BAMFCSV_module;
+VALUE BAMFCSV_MalformedCSVError_class;
 
-  struct bamfcsv_Cell *new_cell = malloc(sizeof(struct bamfcsv_Cell));
+VALUE bamfcsv_finalize_cell(char *cell_start, char *cell_end, int quote_count) {
+  VALUE dbl_dquote = rb_str_new("\"\"", 2), dquote = rb_str_new("\"", 1);
+  ID gsub_bang = rb_intern("gsub!");
 
-  new_cell -> start = NULL;
-  new_cell -> len = 0;
-  new_cell -> next_cell = NULL;
-  new_cell -> has_quotes = 0;
-  row->cell_count++;
-  if (prev_cell != NULL) prev_cell->next_cell = new_cell;
+  if (cell_start == cell_end)
+    return Qnil;
 
-  return new_cell;
+  if (*cell_end == '\r')
+    cell_end--;
 
+  VALUE cell_str = rb_str_new(cell_start, cell_end-cell_start+1);
+
+  if (quote_count)
+    rb_funcall(cell_str, gsub_bang, 2, dbl_dquote, dquote);
+
+  return cell_str;
 }
 
-struct bamfcsv_Row *bamfcsv_alloc_row(struct bamfcsv_Row *prev_row) {
-
-  struct bamfcsv_Row *new_row = malloc(sizeof(struct bamfcsv_Row));
-
-  new_row -> next_row = NULL;
-  new_row -> cell_count = 0;
-  new_row -> first_cell = bamfcsv_alloc_cell(new_row, NULL);
-  if (prev_row != NULL) prev_row->next_row = new_row;
-
-  return new_row;
-
-}
-
-void bamfcsv_free_rows(struct bamfcsv_Row *row) {
-
-  struct bamfcsv_Row *cur_row = row;
-  while (cur_row != NULL) {
-    struct bamfcsv_Cell *cur_cell = cur_row->first_cell;
-    while (cur_cell != NULL) {
-      struct bamfcsv_Cell *next_cell = cur_cell->next_cell;
-      free(cur_cell);
-      cur_cell = next_cell;
-    }
-    struct bamfcsv_Row *next_row = cur_row->next_row;
-    free(cur_row);
-    cur_row = next_row;
-  }
-
-}
-
-VALUE bamfcsv_build_matrix_from_pointer_tree(struct bamfcsv_Row *first_row, unsigned long num_rows) {
-  VALUE matrix;
-  VALUE row;
-  VALUE new_string;
-  unsigned long i,j;
-  struct bamfcsv_Row *cur_row;
-  struct bamfcsv_Cell *cur_cell;
-
-  cur_row = first_row;
-  matrix = rb_ary_new2(num_rows);
-
-  ID gsub = rb_intern("gsub!");
-  VALUE dquote = rb_str_new2("\"\""), quote = rb_str_new2("\"");
-
-  for (i = 0; i < num_rows; i++) {
-
-    cur_cell = cur_row->first_cell;
-    row = rb_ary_new2(cur_row->cell_count);
-    rb_ary_store(matrix,i,row);
-    if (cur_row->cell_count > 1 || cur_cell->len) {
-      for (j = 0; j < cur_row->cell_count; j++) {
-        if (cur_cell->has_quotes) {
-          new_string = rb_str_new(cur_cell->start+1, cur_cell->len-2);
-          rb_funcall(new_string, gsub, 2, dquote, quote);
-        } else {
-          if (cur_cell->len)
-            new_string = rb_str_new(cur_cell->start, cur_cell->len);
-          else
-            new_string = Qnil; /* Empty, unquoted cells are nil, for default ruby CSV compatibility */
-        }
-        rb_ary_store(row, j, new_string);
-        cur_cell = cur_cell->next_cell;
-      }
-    }
-    cur_row = cur_row->next_row;
-  }
-
-  return matrix;
-}
-
-void bamfcsv_finalize_cell(struct bamfcsv_Cell *cell, char *cur, int quote_count) {
-  if (*(cur-1) == '\r') 
-    cell->len = (int)(cur-(cell->start)-1);
-  else
-    cell->len = (int)(cur-(cell->start));
-
-  if (quote_count) cell->has_quotes = 1;
-}
-
-VALUE bamfcsv_build_matrix(char *buf, int bufsize) {
-  int str_start = 0;
-  int num_rows = 1;
+VALUE bamfcsv_build_matrix(char *buf, unsigned long bufsize) {
+  unsigned long num_rows = 1, cell_count = 1;
   int quote_count = 0, quotes_matched = 1;
 
-  struct bamfcsv_Row *first_row = bamfcsv_alloc_row(NULL);
-  struct bamfcsv_Row *cur_row = first_row;
-  struct bamfcsv_Cell *cur_cell = cur_row->first_cell;
-  cur_cell->start = buf;
+  VALUE matrix = rb_ary_new();
+  VALUE row = rb_ary_new();
 
-  VALUE matrix;
-
-  char *cur;
+  char *cur = buf, *cell_start = buf;
 
   if (bufsize > 0 && *(buf+bufsize-1) == '\n') {
     *(buf+bufsize-1) = 0;
     --bufsize;
   }
   
-  for (cur = buf; cur < buf+bufsize; cur++) {
+  for (; cur < buf+bufsize; cur++) {
 
     if (*cur == '"') {
-      if (0 == quote_count && cur_cell->start != cur) /* Quotes begin past opening of cell */
-        rb_raise(BAMFCSV_MalformedCSVError_class, "Illegal quoting on line %d, cell %d: Quoted cell must open with '\"'", num_rows, cur_row->cell_count);
+      if (0 == quote_count && cell_start != cur) /* Quotes begin past opening of cell */
+        rb_raise(BAMFCSV_MalformedCSVError_class, "Illegal quoting on line %lu, cell %lu: Quoted cell must open with '\"'", num_rows, cell_count);
       else
         ++quote_count;
     }
@@ -130,43 +52,45 @@ VALUE bamfcsv_build_matrix(char *buf, int bufsize) {
       if (*cur == ',') {
         
         if (quote_count && *(cur-1) != '"')
-          rb_raise(BAMFCSV_MalformedCSVError_class, "Unclosed quoted field on line %d, cell %d.", num_rows, cur_row->cell_count);
+          rb_raise(BAMFCSV_MalformedCSVError_class, "Unclosed quoted field on line %lu, cell %lu.", num_rows, cell_count);
 
-        bamfcsv_finalize_cell(cur_cell, cur, quote_count);
-        cur_cell = bamfcsv_alloc_cell(cur_row, cur_cell);
-        cur_cell->start = cur+1;
+        VALUE cell_str = bamfcsv_finalize_cell(cell_start, cur-1, quote_count);
+        rb_ary_push(row, cell_str);
+        cell_start = cur+1;
+
         quote_count = 0;
+        ++cell_count;
 
       } else if (*cur == '\n') {
         
         if (quote_count && !(*(cur-1) == '"' || *(cur-1) == '\r' && *(cur-2) == '"'))
-            rb_raise(BAMFCSV_MalformedCSVError_class, "Unclosed quoted field on line %d, cell %d: EOL", num_rows, cur_row->cell_count);
+            rb_raise(BAMFCSV_MalformedCSVError_class, "Unclosed quoted field on line %lu, cell %lu: EOL", num_rows, cell_count);
 
-        bamfcsv_finalize_cell(cur_cell, cur, quote_count);
-        cur_row = bamfcsv_alloc_row(cur_row);
-        cur_cell = cur_row->first_cell;
-        cur_cell->start = cur+1;
-        quote_count = 0;
+        VALUE cell_str = bamfcsv_finalize_cell(cell_start, cur-1, quote_count);
+        rb_ary_push(row, cell_str);
+        rb_ary_push(matrix, row);
+        row = rb_ary_new();
+        cell_start = cur+1;
         
-        num_rows++;
+        quote_count = 0;
+        ++num_rows;
+        cell_count = 0;
 
       } else if (quote_count && *cur != '\r' && *cur != '"')
-        rb_raise(BAMFCSV_MalformedCSVError_class, "Illegal quoting on line %d, cell %d", num_rows, cur_row->cell_count);
+        rb_raise(BAMFCSV_MalformedCSVError_class, "Illegal quoting on line %lu, cell %lu", num_rows, cell_count);
       
     }
 
   }
 
   if (!quotes_matched) /* Reached EOF without matching quotes */
-    rb_raise(BAMFCSV_MalformedCSVError_class, "Illegal quoting on line %d, cell %d: File ends without closing '\"'", num_rows, cur_row->cell_count);
+    rb_raise(BAMFCSV_MalformedCSVError_class, "Illegal quoting on line %lu, cell %lu: File ends without closing '\"'", num_rows, cell_count);
   else if (quote_count && *(cur-1) != '"') /* Quotes closed before end of final cell */
-    rb_raise(BAMFCSV_MalformedCSVError_class, "Unclosed quoted field on line %d, cell %d: EOF", num_rows, cur_row->cell_count);
+    rb_raise(BAMFCSV_MalformedCSVError_class, "Unclosed quoted field on line %lu, cell %lu: EOF", num_rows, cell_count);
 
-  bamfcsv_finalize_cell(cur_cell, cur, quote_count);
-
-  matrix = bamfcsv_build_matrix_from_pointer_tree(first_row, num_rows);
-
-  bamfcsv_free_rows(first_row);
+  VALUE cell_str = bamfcsv_finalize_cell(cell_start, cur-1, quote_count);
+  rb_ary_push(row, cell_str);
+  rb_ary_push(matrix, row);
 
   return matrix;
 
@@ -174,7 +98,7 @@ VALUE bamfcsv_build_matrix(char *buf, int bufsize) {
 
 VALUE bamfcsv_parse_string(VALUE self, VALUE string) {
 
-  return bamfcsv_build_matrix(RSTRING_PTR(string), NUM2INT(rb_str_length(string)));
+  return bamfcsv_build_matrix(RSTRING_PTR(string), NUM2ULONG(rb_str_length(string)));
 
 }
 
